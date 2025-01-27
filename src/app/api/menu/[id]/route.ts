0a509defae3aadf3b38f6730e1b8178ec3df9f68
Mariusfoko <tremolojones.79@yahoo.com>
2025-01-27 08:31:47 +0100

    Correction de l'erreur de type dans le fichier route.ts

--- src/app/api/menu/[id]/route.ts
+++ src/app/api/menu/[id]/route.ts
@@ -2,9 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { menu } from '../../../data/data';
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const food = menu.find(
-        (item: { id: number }) => item.id.toString() === params.id
-    );
+    const { id } = params; // Déstructuration de params pour obtenir directement id
+    const food = menu.find((item: { id: number }) => item.id.toString() === id);
 
     if (!food) {
         return NextResponse.json({ error: 'Item not found' }, { status: 404 });
